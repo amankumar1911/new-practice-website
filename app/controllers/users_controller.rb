102042688aff89a23af0b6e091569a9ecac0bc11
Aman kumar <amank94300@gmail.com>
2021-12-15 13:36:07 +0530

    Creating New User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,10 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 	end
+	def new
+		# @users = User.new
+	end
+	def create
+		render plain: params[:user]
+	end
 end
