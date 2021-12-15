9045c791e435566e6f3498282ebf627744742dd2
Aman kumar <amank94300@gmail.com>
2021-12-15 21:11:13 +0530

    Validate user and flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,23 @@ class UsersController < ApplicationController
 	def index
 		@users = User.all
 	end
+
 	def show
 		@user = User.find(params[:id])
 	end
+
 	def new
-		# @users = User.new
+		@user = User.new
 	end
+
 	def create
 		# render plain: params[:user]
 		@user = User.new(params.require(:user).permit(:name, :email, :address))
-		@user.save
-		redirect_to @user
+		if @user.save
+			flash[:notice] = "User registered Successfully"
+			redirect_to @user
+		else
+			render 'new'
+		end
 	end
 end
