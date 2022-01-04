b2db4d0eb5f5d70eac62d850f559623b3c84967d
Aman kumar <amank94300@gmail.com>
2022-01-04 17:50:43 +0530

    Added Users Sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,41 +2,41 @@ class UsersController < ApplicationController
 
 	before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-	def index
-		@users = User.all
-	end
+	# def index
+	# 	@users = User.all
+	# end
 
-	def show
-	end
+	# def show
+	# end
 
 	def new
 		@user = User.new
 	end
 
 
-	def edit
-	end
+	# def edit
+	# end
 
-	def update
-		if @user.update(user_param)
-			flash[:notice] = "User Edited Successfully"
-			redirect_to @user
-		else
-			render 'edit'
-		end
-	end
+	# def update
+	# 	if @user.update(user_param)
+	# 		flash[:notice] = "User Edited Successfully"
+	# 		redirect_to @user
+	# 	else
+	# 		render 'edit'
+	# 	end
+	# end
 
-	def destroy
-		@user.destroy
-		redirect_to users_path
-	end
+	# def destroy
+	# 	@user.destroy
+	# 	redirect_to users_path
+	# end
 
 	def create
 		# render plain: params[:user]
 		@user = User.new(user_param)
 		if @user.save
-			flash[:notice] = "User registered Successfully"
-			redirect_to @user
+			flash[:notice] = "Welcome to Alpha blog #{user.username}, you have successfully signed up"
+			redirect_to articles_path
 		else
 			render 'new'
 		end
@@ -44,12 +44,12 @@ class UsersController < ApplicationController
 
 	private
 
-	def set_user
-		@user = User.find(params[:id])
-	end
+	# def set_user
+	# 	@user = User.find(params[:id])
+	# end
 
 	def user_param
-		params.require(:user).permit(:username, :email)
+		params.require(:user).permit(:username, :email, :password)
 	end
 
 end
